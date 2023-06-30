94de6e6e7a3627be06f9a4e605f8a256b6f2b41f
Rodrigo Yanez <wintermute.ryf@gmail.com>
2023-06-29 21:48:36 -0600

    first commit

---
+++ members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "Bob bobson",
+    email: "bob@gmail.com",
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "Don bobson",
+    email: "don@gmail.com",
+    status: "active",
+  },
+  {
+    id: 3,
+    name: "John bobson",
+    email: "john@gmail.com",
+    status: "active",
+  },
+];
+
+module.exports = members;
